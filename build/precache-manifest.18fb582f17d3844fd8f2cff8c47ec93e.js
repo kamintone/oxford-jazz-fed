d3e456ecbe9514ba0a70e1124e1baa2e741f3dd7
Tony Davis <tony@tony-davis.co.uk>
2019-06-18 16:23:48 +0100

    Restore imports in Header.js

---
+++ build/precache-manifest.18fb582f17d3844fd8f2cff8c47ec93e.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "27261346aa308cbe27f6",
+    "url": "/static/js/main.a373e59a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ffa73032a7b37660bcec",
+    "url": "/static/css/2.37d96f7f.chunk.css"
+  },
+  {
+    "revision": "ffa73032a7b37660bcec",
+    "url": "/static/js/2.8d2b7240.chunk.js"
+  },
+  {
+    "revision": "d638f56a77526f6edb5195504ca34fc4",
+    "url": "/static/media/hildward.d638f56a.jpg"
+  },
+  {
+    "revision": "34eab5fcfd4f92fa9b6f821903ffafed",
+    "url": "/static/media/alyn-link.34eab5fc.png"
+  },
+  {
+    "revision": "f4ead878ff64e0cb22f04f2b4f84c008",
+    "url": "/static/media/alvin-link.f4ead878.png"
+  },
+  {
+    "revision": "e510000ac440aade8a90dd5cd89b7fc5",
+    "url": "/static/media/dave-link.e510000a.png"
+  },
+  {
+    "revision": "85c491ade3544424c438a7a543657f0c",
+    "url": "/static/media/jude-link.85c491ad.png"
+  },
+  {
+    "revision": "af093d70cd9c8b31762eeaa65b1d3246",
+    "url": "/static/media/nick-gill-link.af093d70.png"
+  },
+  {
+    "revision": "102c6b0c4c6a4d3f19808ca1d342420a",
+    "url": "/static/media/sandy-link.102c6b0c.png"
+  },
+  {
+    "revision": "a575b4cddb1874c49add3bba1effe732",
+    "url": "/static/media/sax-heaven-link.a575b4cd.png"
+  },
+  {
+    "revision": "5a0066bfd0303edf2f9b9afdff227d06",
+    "url": "/static/media/shawnuff-link.5a0066bf.png"
+  },
+  {
+    "revision": "fd94adff3a1c588fca2729284414b152",
+    "url": "/static/media/slam-link.fd94adff.png"
+  },
+  {
+    "revision": "3eca0a76f1ef593c8bdc56db6416baf8",
+    "url": "/static/media/tony-link.3eca0a76.png"
+  },
+  {
+    "revision": "95cc046200357e78707d3721c3015c60",
+    "url": "/static/media/jf-page-1.95cc0462.jpg"
+  },
+  {
+    "revision": "e6261f0d4fed3803ee1026998dea9615",
+    "url": "/static/media/jf-page-2.e6261f0d.jpg"
+  },
+  {
+    "revision": "f761c5b47af103fb37db4ebbf5775d75",
+    "url": "/static/media/big-band.f761c5b4.jpg"
+  },
+  {
+    "revision": "04927122824390d5e5c57169fe4153a9",
+    "url": "/static/media/alan.04927122.jpg"
+  },
+  {
+    "revision": "3d9234974b51ed1e701d86a1ae745429",
+    "url": "/static/media/galvin-stakla.3d923497.png"
+  },
+  {
+    "revision": "5953915fad76679b8dc09e0bde3af2d0",
+    "url": "/index.html"
+  }
+];
