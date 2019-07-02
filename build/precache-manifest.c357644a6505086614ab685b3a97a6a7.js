68283e5213f55484135603e26dbe9fcce34f0e89
Tony Davis <tony@tony-davis.co.uk>
2019-07-02 16:16:37 +0100

    Add build files - don't know why

---
+++ build/precache-manifest.c357644a6505086614ab685b3a97a6a7.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a962731d25a6d5b7bd8",
+    "url": "/static/js/main.1ff2e5f6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "47edb71e28059fabb5e8",
+    "url": "/static/css/2.37d96f7f.chunk.css"
+  },
+  {
+    "revision": "47edb71e28059fabb5e8",
+    "url": "/static/js/2.205b316a.chunk.js"
+  },
+  {
+    "revision": "d638f56a77526f6edb5195504ca34fc4",
+    "url": "/static/media/hildward.d638f56a.jpg"
+  },
+  {
+    "revision": "34eab5fcfd4f92fa9b6f821903ffafed",
+    "url": "/static/media/alyn-link.34eab5fc.png"
+  },
+  {
+    "revision": "f4ead878ff64e0cb22f04f2b4f84c008",
+    "url": "/static/media/alvin-link.f4ead878.png"
+  },
+  {
+    "revision": "e510000ac440aade8a90dd5cd89b7fc5",
+    "url": "/static/media/dave-link.e510000a.png"
+  },
+  {
+    "revision": "85c491ade3544424c438a7a543657f0c",
+    "url": "/static/media/jude-link.85c491ad.png"
+  },
+  {
+    "revision": "af093d70cd9c8b31762eeaa65b1d3246",
+    "url": "/static/media/nick-gill-link.af093d70.png"
+  },
+  {
+    "revision": "fcc734f03d0712e26d77cf34a0d7c3a7",
+    "url": "/static/media/whitley-bay-link.fcc734f0.png"
+  },
+  {
+    "revision": "102c6b0c4c6a4d3f19808ca1d342420a",
+    "url": "/static/media/sandy-link.102c6b0c.png"
+  },
+  {
+    "revision": "a575b4cddb1874c49add3bba1effe732",
+    "url": "/static/media/sax-heaven-link.a575b4cd.png"
+  },
+  {
+    "revision": "5a0066bfd0303edf2f9b9afdff227d06",
+    "url": "/static/media/shawnuff-link.5a0066bf.png"
+  },
+  {
+    "revision": "fd94adff3a1c588fca2729284414b152",
+    "url": "/static/media/slam-link.fd94adff.png"
+  },
+  {
+    "revision": "3eca0a76f1ef593c8bdc56db6416baf8",
+    "url": "/static/media/tony-link.3eca0a76.png"
+  },
+  {
+    "revision": "95cc046200357e78707d3721c3015c60",
+    "url": "/static/media/jf-page-1.95cc0462.jpg"
+  },
+  {
+    "revision": "e6261f0d4fed3803ee1026998dea9615",
+    "url": "/static/media/jf-page-2.e6261f0d.jpg"
+  },
+  {
+    "revision": "056e873434f8330bd5b9df56cff53ed6",
+    "url": "/static/media/jf-page-3.056e8734.jpg"
+  },
+  {
+    "revision": "f5fccbef0eb7da3e7e9e930397a19319",
+    "url": "/static/media/jf-page-4.f5fccbef.jpg"
+  },
+  {
+    "revision": "f761c5b47af103fb37db4ebbf5775d75",
+    "url": "/static/media/big-band.f761c5b4.jpg"
+  },
+  {
+    "revision": "04927122824390d5e5c57169fe4153a9",
+    "url": "/static/media/alan.04927122.jpg"
+  },
+  {
+    "revision": "df2eb69bf609f43f1841c8b9b5869397",
+    "url": "/index.html"
+  }
+];
