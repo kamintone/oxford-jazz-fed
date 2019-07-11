79ae8f24576983c064c052abb7503e12c5a039a2
Tony Davis <tony@tony-davis.co.uk>
2019-07-11 18:48:35 +0100

    Display keys for testing

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ app.use(express.static(path.resolve(__dirname, '.', 'build')))
 //routes
 app.get('/api/memberList', (_req, res) => {
   console.log("url: ", url)
-  // console.log("api-key: ", mc_api_key)
-  // console.log("list id: ", list_id)
+  console.log("api-key: ", mc_api_key)
+  console.log("list id: ", list_id)
   mailchimp.get(`/lists/${list_id}/members?count=100`)
   .then(function(results){
     res.send(results)
